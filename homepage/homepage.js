748ca0d0fe48c1d2eac5c04a5c6db37b504bde29
AlexMolo12 <82962344+AlexMolo12@users.noreply.github.com>
2021-04-26 22:24:35 +0200

    Delete homepage directory

--- homepage/homepage.js
+++
@@ -1,9 +0,0 @@
-function menuComparsa(id){
-
-    var element = document.getElementById(id).style;
-    if(element.display == "none"){
-        element.display = "block";
-    }else{
-        element.display = "none";
-    }
-}
